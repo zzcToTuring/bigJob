671c6d7ee1f4d763b22267f60866400aa5c0bbba
zzc <zzc_wx@qq.com>
2024-11-24 19:13:59 +0800

    网页设计

---
+++ shareLove-网页设计/JavaScript/player.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+//播放器的设置
+window.onload=function(){
+    var btn=document.getElementsByClassName("music-btn")[0];
+    var ply=document.getElementById("music");
+    var srt= document.getElementsByClassName("start")[0];
+    var stp= document.getElementsByClassName("stop")[0];
+	if (ply.paused) {
+            //console.log("暂停");
+            srt.style.display = "block";
+            stp.style.display = "none";
+            ply.play();
+            btn.style.backgroundPosition = "-179px -194px";
+        }
+    btn.onclick = function() {
+        console.log("单击");
+        if (ply.paused) {
+          //  console.log("暂停");
+            srt.style.display = "block";
+            stp.style.display = "none";
+            ply.play();
+            btn.style.backgroundPosition = "-179px -194px";
+        }
+        else {
+           // console.log("播放");
+            srt.style.display = "none";
+            stp.style.display = "block";
+            ply.pause();
+            btn.style.backgroundPosition = "-137px -153px";
+        }
+    }
+}
